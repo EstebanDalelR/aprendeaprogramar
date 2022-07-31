9bead1d06b86c75fbbd7103caf1427e195750f4e
estebandalelr <estebandalelr@gmail.com>
2022-07-31 14:31:56 -0500

    Add content to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,1 +1,38 @@
-export default function Navbar() {}
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <img src="/logo.png" alt="Logo" />
+        </a>
+      </div>
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item" href="/">
+            Inicio
+          </a>
+          <a className="navbar-item" href="/about">
+            Acerca de
+          </a>
+          <a className="navbar-item" href="/contact">
+            Contacto
+          </a>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <a className="button is-primary" href="/">
+                  <span className="icon">
+                    <i className="fab fa-github"></i>
+                  </span>
+                  <span>GitHub</span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
