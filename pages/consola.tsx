7c5faa8ff5a7edd9b904fa87f1727f2f9c9b3122
Esteban Dalel R <estebandalelr@gmail.com>
2022-07-31 14:22:34 -0500

    Added link to start with console

---
+++ pages/consola.tsx
@@ -0,0 +1,11 @@
+export default function Consola() {
+  return (
+    <div>
+      <h1>Consola</h1>
+      <p>
+        Esta es la consola de la aplicación. Aquí podrás ver los errores
+        producidos en la aplicación.
+      </p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Aprende a Programar con Esteban Dalel R</title>
-        <meta name="description" content="Aprende desde cero a programar para la vida" />
+        <meta
+          name="description"
+          content="Aprende desde cero a programar para la vida"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        Aprende a Programar con <a href="https://estebandalelr.co">Esteban Dalel R</a>
+          Aprende a Programar con <wbr></wbr>
+          <a href="https://estebandalelr.co">Esteban Dalel R</a>
         </h1>
-
+        <p className={styles.description}>
+          Aprende desde cero a programar para la vida
+        </p>
+        <Link href="consola">
+          <a className={styles.a}>Empieza ahora</a>
+        </Link>
       </main>
 
       <footer className={styles.footer}>
@@ -28,5 +38,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
