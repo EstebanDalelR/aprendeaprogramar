732ea6df174528ea8d5c6d338c8d3eaefc260837
estebandalelr <estebandalelr@gmail.com>
2022-08-03 11:57:33 -0500

    Make sidebar mobile friendly

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,20 @@
 import Link from "next/link";
 import pages from "../data/pages.json";
 export default function Navbar() {
+  const showNavbar = () => {
+    const navbar = document.querySelector(".navbar-list");
+    if (navbar.classList.contains("show")) {
+      navbar.classList.remove("show");
+    } else {
+      navbar.classList.add("show");
+    }
+  };
+
   return (
     <nav className="navbar">
+      <button className="navbar-button" onClick={() => showNavbar()}>
+        =
+      </button>
       <ol className="navbar-list">
         {pages.map((page) => (
           <li className="navbar-item" key={page.title}>
